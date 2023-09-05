6feb481077f5b5780a208820636332d3fce17d21
FrancoLacourt <134007479+FrancoLacourt@users.noreply.github.com>
2023-09-05 16:50:15 -0300

    InheritanceExercises

--- Ejercicios_Java/InheritanceExercises_Java/Exercise03Inheritance/exercise03inheritance/Exercise03Inheritance.java
+++ Ejercicios_Java/InheritanceExercises_Java/Exercise03Inheritance/exercise03inheritance/Exercise03Inheritance.java
@@ -194,3 +194,14 @@ public class Exercise03Inheritance {
         }
     }
 }
+
+/*
+Siguiendo el ejercicio anterior, en el main vamos a crear un ArrayList de Electrodomésticos
+para guardar 4 electrodomésticos, ya sean lavadoras o televisores, con valores ya asignados.
+Luego, recorrer este array y ejecutar el método precioFinal() en cada electrodoméstico.
+Se deberá también mostrar el precio de cada tipo de objeto, es decir, el precio de todos los
+televisores y el de las lavadoras. Una vez hecho eso, también deberemos mostrar, la suma del
+precio de todos los Electrodomésticos. Por ejemplo, si tenemos una lavadora con un precio de
+2000 y un televisor de 5000, el resultado final será de 7000 (2000+5000) para
+electrodomésticos, 2000 para lavadora y 5000 para televisor.
+*/
